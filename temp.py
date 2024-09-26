4926afdaccfdc209b4ea7f6e691305687cdf9cd6
SarahAnberlin <sarahanberlin@outlook.com>
2024-09-26 10:27:31 +0800

    ?

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 from torchvision import transforms
+import torch
 
 from hubconf import dinov2_vits14
 
@@ -9,14 +10,18 @@ img_root = f'/dataset/vfayezzhang/dataset/MiniImageNet1k'
 files = [f for f in os.listdir(img_root) if f.endswith('.jpg') or f.endswith('.png')]
 file_paths = [os.path.join(img_root, f) for f in files]
 
-model = dinov2_vits14()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = dinov2_vits14().to(device).eval()
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+])
+
 for file_path in file_paths:
     image = Image.open(file_path).convert('RGB')
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-    ])
     image = transform(image).unsqueeze(0)
     feature = model(image, is_training=True)
     print(f"Type of feature: {type(feature)}")
